fb76c752ad060b1d8e808f31d6fc678b437e798e
nba18 <nba18012001@gmail.com>
2023-02-17 22:51:35 +0700

    aa

--- frontend-luanvan/src/components/detai/chitietdetai.jsx
+++ frontend-luanvan/src/components/detai/chitietdetai.jsx
@@ -20,36 +20,36 @@ function Chitietdetai(props) {
     return (
         <div>
             <div className="bg-[#2A84EB] rounded-lg h-10 w-[73rem] p-2 m-auto translate-y-6 shadow-lg shadow-blue-500/50 text-center text-white font-bold" >Chi tiết đề tài</div>
-            <div className="w-[73rem] p-4 m-auto mt-5">
-                <div className='my-5'>
+            <div className="w-[75rem] m-auto rounded-lg bg-white shadow-lg pt-5">
+                <div className='my-5 ml-10 mr-10 '>
                     <div className='w-1/4 font-bold'>Tên đề tài</div>
                     <Markup content={tendetai} />
                 </div>
-                <div className='my-5'>
+                <div className='my-5 ml-10 mr-10 '>
                     <div className='w-1/4 font-bold'>Tên đề tài bằng tiếng Anh:</div>
                     <Markup content={tentienganh} />
                 </div>
-                <div className='my-4 '>
+                <div className='my-4 ml-10 mr-10  '>
                     <div className='font-bold'>Sinh viên thực hiện</div>
                     <div></div>
                 </div>
-                <div className='my-4'>
+                <div className='my-4 ml-10 mr-10 '>
                     <div className='font-bold'>1. Yêu cầu kiến thức</div>
                     <Markup content={mota_kienthuc} />
                 </div>
-                <div className='my-4'>
+                <div className='my-4 ml-10 mr-10 '>
                     <div className='font-bold'>2. Giới thiệu</div>
                     <Markup content={mota_gioithieu} />
                 </div>
-                <div className='my-4'>
+                <div className='my-4 ml-10 mr-10 '>
                     <div className='font-bold'>3. Mục tiêu và yêu cầu chức năng</div>
                     <Markup content={mota_yeucau} />
                 </div>
-                <div className='my-4'>
+                <div className='my-4 ml-10 '>
                     <div className='font-bold'>4. Tài liệu tham khảo</div>
                     <Markup content={mota_tailieu} />
                 </div>
-                <div className='my-4'>
+                <div className='my-4 ml-10 mr-10 pb-5'>
                     <div className='font-bold'>5. Yêu cầu khác</div>
                     <Markup content={mota_khac} />
                 </div>
